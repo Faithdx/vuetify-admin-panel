8598f1fe3c80dd189258289abe3b5910df8d7bcd
hhh <2375740452@qq.com>
2021-07-25 01:42:27 +0800

    UI Style

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,32 +1,32 @@
 <template>
-  <v-row justify="center" class="m-style">
-    <v-card class="mx-auto">
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="../assets/images/register/bg_reg-1@3x.png"
-      >
-        <v-app-bar flat color="rgba(0, 0, 0, 0)">
-          <v-app-bar-nav-icon color="white">
-            <v-icon>mdi-reply</v-icon>
-          </v-app-bar-nav-icon>
-          <v-spacer></v-spacer>
-          <v-btn color="white" icon>
-            <span>로그인</span>
-          </v-btn>
-        </v-app-bar>
-        <v-card-title class="flex-column align-center white--text mt-2">
-          <v-avatar size="56">
-            <img
-              alt="title"
-              src="../assets/images/register/icon_title@3x.png"
-            />
-          </v-avatar>
-          <span>회원가입</span>
-        </v-card-title>
-      </v-img>
-      <v-card-text>
-        <v-container fluid>
+  <v-container fluid>
+    <v-row justify="center" class="m-style">
+      <v-card class="mx-auto card-box">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="../assets/images/register/bg_reg-1@3x.png"
+        >
+          <v-app-bar flat color="rgba(0, 0, 0, 0)">
+            <v-app-bar-nav-icon color="white">
+              <v-icon>mdi-reply</v-icon>
+            </v-app-bar-nav-icon>
+            <v-spacer></v-spacer>
+            <v-btn color="white" icon>
+              <span>로그인</span>
+            </v-btn>
+          </v-app-bar>
+          <v-card-title class="flex-column align-center white--text mt-2">
+            <v-avatar size="56">
+              <img
+                alt="title"
+                src="../assets/images/register/icon_title@3x.png"
+              />
+            </v-avatar>
+            <span>회원가입</span>
+          </v-card-title>
+        </v-img>
+        <v-card-text class="pb-2">
           <v-form
             ref="registerForm"
             v-model="valid"
@@ -34,29 +34,25 @@
             autocomplete="off"
           >
             <v-row>
-              <v-col>
+              <v-col class="pb-0">
+                <h5 class="mb-2 font-color">账户信息</h5>
                 <v-text-field
+                  outlined
+                  dense
                   v-model="form.username"
                   :rules="rules.usernameRules"
-                  label="用户名"
+                  label="ID"
                 ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
                 <v-text-field
-                  v-model="form.password"
-                  :rules="rules.passwordRules"
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
                   label="密码"
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="show = !show"
-                  :type="show ? 'text' : 'password'"
                 ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
                 <v-text-field
+                  outlined
+                  dense
                   v-model="form.passwordConfirm"
                   :rules="[
                     rules.passwordConfirmRules.required,
@@ -70,22 +66,113 @@
                   @click:append="show = !show"
                   :type="show ? 'text' : 'password'"
                 ></v-text-field>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="订单密码"
+                ></v-text-field>
+                <v-text-field
+                  class="mb-0"
+                  outlined
+                  dense
+                  v-model="form.passwordConfirm"
+                  :rules="[
+                    rules.passwordConfirmRules.required,
+                    rules.passwordConfirmRules.equal(
+                      form.password,
+                      form.passwordConfirm
+                    ),
+                  ]"
+                  label="确认订单密码"
+                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="show = !show"
+                  :type="show ? 'text' : 'password'"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row class="mt-0">
+              <v-col class="pb-0">
+                <h5 class="mb-2 font-color">隐私</h5>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="名称"
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="手机"
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="出生日期"
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="电子邮件"
+                ></v-text-field>
+                <v-text-field
+                  class="mb-0"
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="家庭地址"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row class="mt-0">
+              <v-col class="pb-0">
+                <h5 class="mb-2 font-color">银行账户信息</h5>
+                <v-select
+                  :items="items"
+                  label="银行名称"
+                  dense
+                  outlined
+                ></v-select>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="账户持有人"
+                ></v-text-field>
+                <v-text-field
+                  outlined
+                  dense
+                  v-model="form.username"
+                  :rules="rules.usernameRules"
+                  label="账号"
+                ></v-text-field>
               </v-col>
             </v-row>
           </v-form>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn :disabled="!valid" color="primary">Register</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-row>
+        </v-card-text>
+        <v-card-actions class="px-4 pb-8">
+          <v-btn :disabled="!valid" rounded class="bg-btn">Register</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
   name: "Register",
   data: () => ({
+    //下拉框数据
+    items: ["交通银行", "建设银行", "人民银行", "平安银行"],
     // 用于验证表单内容填写是否正确
     valid: true,
     // 用于控制密码是否以可见的形式显示
@@ -120,7 +207,19 @@ export default {
 };
 </script>
 <style scoped>
-.m-style{
-  margin: -32px -24px;
+.m-style {
+  margin: -44px -36px;
+}
+.card-box {
+  box-shadow: none !important;
+  border-radius: 0 !important;
+}
+.font-color {
+  color: #cb0682;
+}
+.bg-btn{
+  background-color: #cb0682 !important;
+  color: #ffffff;
+  width: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
   },
   {
     path: '/register',
-    name: 'Register',
+    name: 'Register', 
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
