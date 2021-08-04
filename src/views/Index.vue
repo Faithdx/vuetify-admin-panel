aa573eb95628e31005de67ff6ca84a3188fc53f6
daxin <daxin@zasoft.cn>
2021-08-04 16:15:23 +0800

    aa

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="index">
     <h1>{{ message }}</h1>
+    <img v-bind:src="imgURL" alt="" />
     <ul>
-      <li v-for="(item, index) in movies" :key="index">{{ item.message }}</li>
-      <h2>当前计数：{{ counter }}</h2>
-      <v-btn color="primary" v-on:click="add"> ++ </v-btn>
-      <v-btn color="error" @click="sub"> -- </v-btn>
+      <li v-for="(item, index) in movies" :key="index" >{{item}}</li>
     </ul>
   </div>
 </template>
@@ -14,12 +12,9 @@
 export default {
   name: "Index",
   data: () => ({
-    message: "what are you doing?",
-    movies: [
-      { message: "啊啊啊啊" },
-      { message: "大话西游" },
-      { message: "你在干什么?？" },
-    ],
+    message: 'what are you doing?',
+    imgURL: 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4769961793e644ab8c12e993948fdee1~tplv-k3u1fbpfcp-watermark.image',
+    movies: ['秦时明月','天行九歌','玲珑','少年歌行'],
     counter: 0,
   }),
   methods: {
@@ -32,5 +27,6 @@ export default {
   },
 };
 </script>
+
 <style lang="scss">
 </style>
