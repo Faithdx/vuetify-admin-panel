0955a95a3dbf6e8a3635f6078421c95bdc5bcace
daxin <1481087974@qq.com>
2022-03-01 17:56:00 +0800

    aaa

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,25 @@
 <template>
-
   <nav>
-
     <v-snackbar v-model="snackbar" :timeout="4000" top color="success">
       <span>Wow! You added a new project.</span>
       <v-btn flat color="white" @click="snackbar = false">Close</v-btn>
     </v-snackbar>
 
     <v-toolbar flat app>
-
       <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
-
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Dev</span>
       </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-      
-
+      <v-spacer></v-spacer>  
       <v-btn flat color="grey" @click="signout">
         <span>Sign out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
-
     </v-toolbar>   
-
 
     <!-- Drawer -->
     <v-navigation-drawer v-model="drawer" app class="primary">
-
       <v-layout column align-center>
         <v-flex class="mt-5 text-xs-center">
           <v-avatar size="130">
@@ -43,7 +33,6 @@
           <Popup @projectAdded="snackbar=true" />
         </v-flex>
       </v-layout>
-
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
@@ -52,17 +41,11 @@
           <v-list-tile-content>
             <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
           </v-list-tile-content>
-
         </v-list-tile>
-      </v-list>
-      
+      </v-list>      
     </v-navigation-drawer>
-
-
   </nav>
-
 </template>
-
 
 <script>
 import Popup from './Popup'
@@ -83,7 +66,6 @@ export default {
     }
   },
   methods: {
-
     signout() {
       firebase.auth().signOut().then(() => {
         this.$router.replace('/auth')
@@ -94,7 +76,6 @@ export default {
 
   created() {
     var user = firebase.auth().currentUser;
-
     if (user) {
       this.name = user.displayName;
       if(user.photoURL){
@@ -106,6 +87,5 @@ export default {
       //
     }
   }
-
 }
 </script>
